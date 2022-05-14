7b0a1b30f6cf6f2f836976792c1dd66972405cab
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-14 20:17:26 -0300

    style: deleting old migrations and changing models

--- api/models/coins.js
+++ api/models/coins.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Coins extends Model {
+  class coins extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,13 +13,13 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Coins.init({
+  coins.init({
     coin: DataTypes.STRING,
     fullname: DataTypes.STRING,
     amount: DataTypes.DOUBLE
   }, {
     sequelize,
-    modelName: 'Coins',
+    modelName: 'coins',
   });
-  return Coins;
+  return coins;
 };
--- api/models/wallet.js
+++ api/models/wallet.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Wallet extends Model {
+  class wallet extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,13 +13,13 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Wallet.init({
+  wallet.init({
     nome: DataTypes.STRING,
     cpf: DataTypes.STRING,
     birthdate: DataTypes.DATEONLY
   }, {
     sequelize,
-    modelName: 'Wallet',
+    modelName: 'wallet',
   });
-  return Wallet;
+  return wallet;
 };
