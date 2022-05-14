8ce59d552eb06a30c132f578895ae3b11c5d9fcb
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-14 20:09:19 -0300

    feat: adding new migrations

---
+++ api/migrations/20220514212041-create-transactions.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      value: {
+        type: Sequelize.DOUBLE
+      },
+      datetime: {
+        type: Sequelize.DATE
+      },
+      sendTo: {
+        type: Sequelize.INTEGER
+      },
+      receiveFrom: {
+        type: Sequelize.INTEGER
+      },
+      currentCotation: {
+        type: Sequelize.DOUBLE
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('transactions');
+  }
+};
